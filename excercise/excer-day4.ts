99d705ab7525cf18b1fefc2f777465a5a693a5fb
Arkungking <aryagiffari0@gmail.com>
2025-09-02 16:40:33 +0700

    tambahan excer day4

--- excercise/excer-day4.ts
+++ excercise/excer-day4.ts
@@ -1,41 +1,94 @@
 // SOAL 1 --
 // buat fungsi yang dapat membuat pola segitiga berdasrkan dengan 'height' yang kita berikan
+//nested loop
+
+
+function segitiga (height: number) {
+    console.log(height);
+    for (let i = 1; i <= height; i++) {
+        let row: string = "";
+
+        for(let j = 1; j <= i; j++) {
+            row += j + " ";
+        }
+
+        console.log(row);
+        
+    }
+}
+
+
+//1
+//1 2
+//1 2 3
+
+segitiga(5);
+
+
+// SOAL 2
+
+function segitiga2 (height: number) {
+    console.log(height);
+    let currentNumber: number = 1;
+
+    for (let i = 1; i <= height; i++) {
+        let row: string = "";
+
+        for(let j = 1; j <= i; j++) {
+            if (currentNumber < 10) {
+            row += "0" + currentNumber + " ";
+        } else {
+            row += currentNumber + " ";
+        }
+        currentNumber++
+        
+    }
+    console.log(row);
+    
+}
+}
+segitiga2(10)
 
 
 
 
-// SOAL 2 --
+
+// SOAL 3 --
 // buat fungsi yang bisa me loop angka berdasarkan input yang kita berikan
 // dan mengganti perkalian angka 3 dengan "FIZZ" dan mengganti perkalian angka 5 dengan  "BUZZ"
 // dan jika angka tersebut adalah perkalian 3 dan 5 terganti dengan "FIzzBuzz"
 
-let n: number = 15;
+
 
 function countdown(n: number) {
+
+let message: string = "";
 for (let i = 1; i <= n; i++) {
-    console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+        message += "FizzBuzz" + " ";
     } else if (i % 5 === 0) {
-        console.log("Buzz");
+         message += "Buzz" + " ";
     } else if (i % 3 === 0) {
-        console.log("Fizz");
+         message += "Fizz" + " ";
     } else {
-        console.log(i);
+         message += i + " ";
     }
-    
 }
+console.log(message);
 }
-countdown(n);
+
+countdown(27)
 
 
-// SOAL 3
+
+
+// SOAL 4
 // buat fungsi untuk menghitung BMI
 // formula BMI = weight / (height) kuadrat
 
-let weight: number = 95;
-let height: number = 167 / 100;
-let bmi: number = weight / (height * height);
+let weightkg: number = 900;
+let heightm: number = 167 / 100;
+let bmi: number = weightkg / (heightm * heightm);
 let fixedBmi: any = bmi.toFixed
 console.log(bmi);
 
@@ -50,14 +103,13 @@ function calculator (num : number) {
     } else if(bmi >= 30.0 && bmi <= 39.9) {
         return "very overweigth" 
     } else {
-        console.log("obesity");
-        
+        return "obesity"
     }
 }
 
 console.log(calculator(bmi));
 
-// SOAL 4
+// SOAL 5
 //buat fungsi untuk  menghilangkan angka ganjil dalam array dan mereturn array baru yang hanya berisi angka genap
 
 let scores: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -71,7 +123,7 @@ const evenArray = scores.filter((score, index) => {
 console.log(evenArray);
 
 
-// SOAL 5
+// SOAL 6
 // buat fungsi untuk memisahkan (split) dan merubah string menjadi array of words
 
 const text1 = (text1: string) => {
